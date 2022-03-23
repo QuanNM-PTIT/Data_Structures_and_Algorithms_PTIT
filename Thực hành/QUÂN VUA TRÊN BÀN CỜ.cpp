232421e27e08e40700d2a1596cd06712fb10c974
Jimmy Nguyễn <84088181+cityuongbi@users.noreply.github.com>
2022-03-23 19:10:28 +0700

    Src code

---
+++ Thực hành/QUÂN VUA TRÊN BÀN CỜ.cpp
@@ -0,0 +1,31 @@
+// Created by Nguyễn Mạnh Quân
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+#define sz size()
+#define ll long long
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define FORD(i, a, b) for(int i = a; i >= b; --i)
+#define F(i, a, b) for(int i = a; i < b; ++i)
+#define FD(i, a, b) for(int i = a; i > b; --i)
+#define faster() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define PI 3.141592653589793238
+#define endl '\n'
+
+int main()
+{
+    faster();
+    int s1, s2, f1, f2;
+    cin >> s1 >> s2 >> f1 >> f2;
+    cout << max(abs(s1 - f1), abs(s2 - f2));
+    return 0;
+}
